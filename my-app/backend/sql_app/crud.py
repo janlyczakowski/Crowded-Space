fa3671d2c8728239f0df39709dfe6483dfb5c9b9
lennartfynn <lennartfynnk@gmail.com>
2023-06-19 20:13:50 +0200

    added api functions for purposes

--- my-app/backend/sql_app/crud.py
+++ my-app/backend/sql_app/crud.py
@@ -1,17 +1,29 @@
 from sqlalchemy.orm import Session
 import models
+#1 call active & debris
 
+#2 call actives
 def get_active(db: Session, active_norad_cat_id: int):
     return db.query(models.Active).filter(models.Active.active_norad_cat_id == active_norad_cat_id).first()
 
-def get_actives(db: Session, skip: int = 0, limit: int = 7856):
+def get_actives(db: Session, skip: int = 0, limit: int = 7834):
     return db.query(models.Active).offset(skip).limit(limit).all()
 
-def read_active_by_active_norad_cat_id(db: Session, active_norad_cat_id: int):
-    return db.query(models.Active).filter(models.Active.active_norad_cat_id == active_norad_cat_id).first()
-
-#def read_satcat(db: Session, satcat_object_name: str):
-    #return db.query(models.Satcat).filter(models.Satcat.satcat_object_name == satcat_object_name).first()
-
-#def read_satcats(db: Session, skip: int = 0, limit: int = 500):
-    #return db.query(models.Satcat).offset(skip).limit(limit).all()
+#3 call debris
+#def get_debris(db: Session, skip: int = 0, limit: int = 3453):
+    #return db.query(models.Debris).offset(skip).limit(limit).all()
+#4 call communications
+def get_communication(db: Session, skip: int = 0, limit: int = 6689):
+    return db.query(models.Communications).offset(skip).limit(limit).all()
+#5 call misc
+def get_misc(db: Session, skip: int = 0, limit: int = 148):
+    return db.query(models.Misc).offset(skip).limit(limit).all()
+#6 call nav
+def get_navigation(db: Session, skip: int = 0, limit: int = 320):
+    return db.query(models.Navigation).offset(skip).limit(limit).all()
+#7 call science
+def get_science(db: Session, skip: int = 0, limit: int = 97):
+    return db.query(models.Science).offset(skip).limit(limit).all()
+#8 call weather
+def get_weather(db: Session, skip: int = 0, limit: int = 635):
+    return db.query(models.Weather_ers).offset(skip).limit(limit).all()
--- my-app/backend/sql_app/main.py
+++ my-app/backend/sql_app/main.py
@@ -22,6 +22,7 @@ def get_db():
 
 
 #path operations
+#call active single
 @app.get("/active/{active_norad_cat_id}", response_model=schemas.ActiveBase)
 def read_active(active_norad_cat_id: int, db: Session = Depends(get_db)):
     db_active_norad_cat_id = crud.get_active(db, active_norad_cat_id=active_norad_cat_id)
@@ -30,11 +31,38 @@ def read_active(active_norad_cat_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found")
 
     return db_active_norad_cat_id
-
+#call actives
 @app.get("/active/", response_model=List[schemas.ActiveBase])
 def read_actives(skip: int = 0, limit: int = 7856, db: Session = Depends(get_db)):
     actives = crud.get_actives(db, skip=skip, limit=limit)
     return actives
+#call actives and debris
+#call debris
+#call communications
+@app.get("/communications/", response_model=List[schemas.CommunicationsBase])
+def read_communication(skip: int = 0, limit: int = 6689, db: Session = Depends(get_db)):
+    communication = crud.get_communication(db, skip=skip, limit=limit)
+    return communication
+#call misc
+@app.get("/misc/", response_model=List[schemas.MiscBase])
+def read_misc(skip: int = 0, limit: int = 148, db: Session = Depends(get_db)):
+    misc = crud.get_misc(db, skip=skip, limit=limit)
+    return misc
+#call nav
+@app.get("/nav/", response_model=List[schemas.NavigationBase])
+def read_misc(skip: int = 0, limit: int = 320, db: Session = Depends(get_db)):
+    nav = crud.get_navigation(db, skip=skip, limit=limit)
+    return nav
+#call science
+@app.get("/science/", response_model=List[schemas.ScienceBase])
+def read_science(skip: int = 0, limit: int = 97, db: Session = Depends(get_db)):
+    science = crud.get_science(db, skip=skip, limit=limit)
+    return science
+#call weather
+@app.get("/weather/", response_model=List[schemas.Weather_ersBase])
+def read_weather(skip: int = 0, limit: int = 635, db: Session = Depends(get_db)):
+    weather = crud.get_weather(db, skip=skip, limit=limit)
+    return weather
 
 #start uvicorn
 if __name__ == "__main__":
